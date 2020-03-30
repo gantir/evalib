e34bdd6533655e1d673d83bd3d614c54cd624dce
Ramjee Ganti <code@ramjeeganti.com>
2020-03-30 15:29:57 +0530

    removed print

--- src/evalib/utils.py
+++ src/evalib/utils.py
@@ -3,8 +3,6 @@ import torch
 
 def get_device():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print("Cuda available?", torch.cuda.is_available())
-
     return device
 
 
